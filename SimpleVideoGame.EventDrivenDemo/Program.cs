35f8810176b7a3d23b61c76f9e909a83ceec09e2
xhevat Ibraimi <xhevat.ibraimi@gmail.com>
2020-05-09 12:15:22 +0200

    Initial commit

---
+++ SimpleVideoGame.EventDrivenDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SimpleVideoGame.EventDrivenDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press ESC to exit");
+            ConsoleKeyInfo keyinfo;
+            do
+            {
+                keyinfo = Console.ReadKey();
+
+                switch (keyinfo.Key)
+                {
+                    case ConsoleKey.A:
+                        Console.WriteLine("\nMoving Left");
+                        break;
+                    case ConsoleKey.S:
+                        Console.WriteLine("\nMoving Down");
+                        break;
+                    case ConsoleKey.W:
+                        Console.WriteLine("\nMoving Up");
+                        break;
+                    case ConsoleKey.D:
+                        Console.WriteLine("\nMoving Right");
+                        break;
+                }
+            }
+            while (keyinfo.Key != ConsoleKey.Escape);
+        }
+    }
+}
